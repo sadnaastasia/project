175e3e873768ae70676486d5156fe7b5ff98ccb2
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2023-11-06 02:38:14 +0300

    :)

---
+++ Chapter 2/Lesson 13/homework.js
@@ -0,0 +1,13 @@
+let sum = 0;
+
+while (true) {
+let number = prompt ('Введите число');
+
+    if (number === '' || number === null) break; 
+    if (isNaN(number)===true) {
+        alert('Это не число!'); continue;
+    }
+    sum += +number;
+    
+}   
+alert (sum);
