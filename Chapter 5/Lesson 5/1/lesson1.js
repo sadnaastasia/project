345bd77587885396cfac6845de350d94ca8df58f
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2023-11-27 22:10:58 +0300

    1 задание из урока Методы массивов

---
+++ Chapter 5/Lesson 5/1/lesson1.js
@@ -0,0 +1,11 @@
+function camelize(str) {
+  let arr = str.split('-');
+  for (let key = 1; key < arr.length; key++) {
+    arr[key]= arr[key][0].toUpperCase() + arr[key].slice(1);
+  }
+  str = arr.join('');
+  return str;
+}
+alert(camelize("background-color")) //== 'backgroundColor';
+alert(camelize("list-style-image")) //== 'listStyleImage';
+alert(camelize("-webkit-transition")) //== 'WebkitTransition';
