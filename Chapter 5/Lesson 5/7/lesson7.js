11196b2ec3417f57e8f445577d929d69cdc15095
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2023-11-30 00:24:58 +0300

    7 задание из урока Методы массивов

---
+++ Chapter 5/Lesson 5/7/lesson7.js
@@ -0,0 +1,17 @@
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [vasya, petya, masha];
+let result = [];
+function transformator(user) {
+    for (let i = 0; i < users.length; i++) {
+        result[i] = users[i].name;
+    }
+    return result;
+}
+transformator();
+
+let names = result;
+
+alert(names); // Вася, Петя, Маша
