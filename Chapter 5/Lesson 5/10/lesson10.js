a03f3dd68f1bf03500fe978b8b879875f02c9feb
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2023-12-04 00:55:12 +0300

    10-12 задания из урока Методы массивов

---
+++ Chapter 5/Lesson 5/10/lesson10.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3];
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    alert(arr);
+  }
+  shuffle(arr);
+  shuffle(arr);
+  shuffle(arr);
+  shuffle(arr);
