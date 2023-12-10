f815e8f8baab8990cd5cd6e7120e8651cb74c04c
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2023-12-10 16:07:45 +0300

    4, 5 exercises on posts

---
+++ Chapter 5/Lesson 5/homework2/5/homework.js
@@ -0,0 +1,90 @@
+const posts = [
+  {
+      id: "post1",
+      author: {
+          id: "user1",
+          name: "Ivan",
+      },
+      body: "My first post.",
+      numberOfLikes: 9,
+      comments: [
+          {
+              id: "comment1",
+              author: {
+                  id: "user2",
+                  name: "Pavel",
+              },
+              text: "Cool!",
+              numberOfLikes: 20,
+          },
+          {
+              id: "comment2",
+              author: {
+                  id: "user3",
+                  name: "Anton",
+              },
+              text: "Super!",
+              numberOfLikes: 32,
+          }
+      ],
+  },
+  {
+      id: "post2",
+      author: {
+          id: "user1",
+          name: "Ivan",
+      },
+      body: "Another post.",
+      numberOfLikes: 10,
+      comments: [
+          {
+              id: "comment3",
+              author: {
+                  id: "user4",
+                  name: "Evgenia",
+              },
+              text: "Keep posting!",
+              numberOfLikes: 5,
+          },
+          {
+              id: "comment4",
+              author: {
+                  id: "user2",
+                  name: "Pavel",
+              },
+              text: "Great!",
+              numberOfLikes: 50,
+          }
+      ],
+  },
+  {
+      id: "post3",
+      author: {
+          id: "user1",
+          name: "Ivan",
+      },
+      body: "I've done it!",
+      numberOfLikes: 40,
+      comments: [
+          {
+              id: "comment5",
+              author: {
+                  id: "user5",
+                  name: "Diana",
+              },
+              text: "Congrats!",
+              numberOfLikes: 1,
+          },
+      ],
+  },
+];
+let maxLikes = 0;
+let mostPopularPost = posts.reduce((obj, post) => findMostPopularPost(obj, post), {});
+function findMostPopularPost(obj, post) {
+    sum = post.numberOfLikes;
+    maxLikes = Math.max(sum, maxLikes);
+    if (maxLikes > sum) return obj;
+    obj = post;
+    return obj;
+}
+
