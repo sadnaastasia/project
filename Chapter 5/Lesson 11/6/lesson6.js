a84ca02eb0a8b8dd5a909f7aa0e4d3a61c91ec28
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-01-22 11:58:59 +0300

    Lesson 11 exercise 6 (date and time)

--- Chapter 5/Lesson 11/6/lesson6.js
+++ Chapter 5/Lesson 11/6/lesson6.js
@@ -1,7 +1,7 @@
 function getSecondsToday() {
     let day = new Date();
     let dayStart = new Date().setHours(0, 0, 0, 0);
-    return (day - dayStart) / 1000;
+    return Math.round((day - dayStart) / 1000);
 }
 
 alert(getSecondsToday());
