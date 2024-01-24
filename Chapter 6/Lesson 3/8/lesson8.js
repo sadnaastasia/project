44f178ad110c5e32e2fcb4e07d4c4b4fb1b468f6
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-01-24 23:06:21 +0300

    Lesson 3 Exercise 8

--- Chapter 6/Lesson 3/8/lesson8.js
+++ Chapter 6/Lesson 3/8/lesson8.js
@@ -7,7 +7,7 @@ function inBetween(a, b) {
 function inArray(arr) {
     return function (item) {
         for (let i = 0; i <= arr.length - 1; i++) {
-            if (arr[i] == item) return item;
+            if (arr[i] == item) return true;
         }
     }
 }
