650964b908f5a581f8db324c2e2108cbdd864846
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-01-26 23:48:01 +0300

    Lesson 8

---
+++ Chapter 6/Lesson 8/1/lesson1.js
@@ -0,0 +1,19 @@
+// function printNumbers(from, to) {
+//   let current = from;
+//   timerid = setInterval(function () {
+//     alert(current);
+//     if (current == to) clearInterval(timerid);
+//     current++;
+//   }, 1000);
+// }
+
+function printNumbers(from, to) {
+  let current = from;
+  timerid = setTimeout(function func() {
+    alert(current);
+    if (current < to) setTimeout(func, 1000);
+    current++;
+  }, 1000);
+}
+
+printNumbers(1, 10);
