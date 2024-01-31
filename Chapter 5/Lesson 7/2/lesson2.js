1960fd7c1de26fc4652e9341c8a803455add29f6
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-02-01 01:46:57 +0300

    Map and Set

--- Chapter 5/Lesson 7/2/lesson2.js
+++ Chapter 5/Lesson 7/2/lesson2.js
@@ -1,21 +1,14 @@
 function aclean(arr) {
-  let arr2 = [];
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i].length == arr[j].length) {
-        let a = arr[i].toLowerCase().split('').sort().join('');
-        let b = arr[j].toLowerCase().split('').sort().join('');
-        if (a == b) {
-          arr2.push(arr[i]);
-          arr.splice(i, 1);
-          arr.splice(j - 1, 1);
-        }
-      }
-    }
+  let map = new Map();
+
+  for (let word of arr) {
+    let sorted = word.toLowerCase().split('').sort().join('');
+    map.set(sorted, word);
   }
-  return arr2;
+
+  return Array.from(map.values());
 }
 
 let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
-alert(aclean(arr)); // "nap,teachers,ear" или "PAN,cheaters,era"
+alert( aclean(arr) );
