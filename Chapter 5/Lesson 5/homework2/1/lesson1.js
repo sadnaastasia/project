8e1712d778b189610eaaf36211fdda6aee3a7b2a
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-02-01 15:15:19 +0300

    corrected

--- Chapter 5/Lesson 5/homework2/1/lesson1.js
+++ Chapter 5/Lesson 5/homework2/1/lesson1.js
@@ -9,12 +9,11 @@ const arrayOfIntegers = [2, 5, 1, 4, 9, 6, 3, 7];
 // }
 
 function findMissingNumber(arr) {
-  let sum = arr.reduce((sum, value) => sum += value);
-  let sumN = (1 + Math.max(...arr)) * (arr.length + 1) / 2;
-  arr.sort((a, b) => a - b);
-  return ((1 + arr.length * (arr[1] - arr[0])) != arr[arr.length - 1])
-    ? (1 + arr.length * (arr[1] - arr[0]))
-    : (sumN - sum);
+  let sum = (1 + arr.length + 1) / 2 * (arr.length + 1);
+  for (let i = 0; i < arr.length; i++) {
+    sum -= arr[i];
+  }
+  return sum;
 }
 
 console.log(findMissingNumber(arrayOfIntegers)); // 8
