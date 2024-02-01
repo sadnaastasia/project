7dcf6635ed917cdee3ddb32059975907e57a271a
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-02-01 13:28:45 +0300

    changed

--- Chapter 5/Lesson 11/3/lesson3.js
+++ Chapter 5/Lesson 11/3/lesson3.js
@@ -1,6 +1,12 @@
 function getLocalDay(date) {
-    let day = [7, 1, 2, 3, 4, 5, 6];
-    return day[date.getDay()];
+    let day;
+    if (date.getDay() == 0) {
+        return day = 7;
+    } else {
+        return date.getDay();
+    }
+    // let day = [7, 1, 2, 3, 4, 5, 6];
+    // return day[date.getDay()];
 } 
 
 let date = new Date(2012, 0, 8);  // 8 января 2012 года
