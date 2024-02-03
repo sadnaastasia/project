767657eecbb6c8d9baad3119da7d0ef6ca81563f
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-02-04 02:06:23 +0300

    Exercise 4

--- Chapter 6/Lesson 9/4/lesson4.js
+++ Chapter 6/Lesson 9/4/lesson4.js
@@ -11,10 +11,11 @@ function throttle(f, ms) {
       count++;
       return f.call(this, ...arguments);
     }
-    if (Date.now() - start >= 1000) {
+    if (Date.now() - start > 999) {
+      clearTimeout(timeout);
       start = Date.now();
       return f.call(this, ...arguments);
-    } else if (Date.now() - start < 1000) {
+    } else if (Date.now() - start <= 999) {
       clearTimeout(timeout);
       timeout = setTimeout(() => f.apply(this, arguments), ms - (Date.now() - start));
     }
@@ -27,3 +28,8 @@ let f1000 = throttle(f, 1000);
 f1000(1); // показывает 1
 f1000(2); // (ограничение, 1000 мс ещё нет)
 f1000(3); // (ограничение, 1000 мс ещё нет)
+setTimeout(() => f1000(4), 900);
+setTimeout(() => f1000(5), 1500);
+setTimeout(() => f1000(6), 1800);
+setTimeout(() => f1000(7), 3000);
+
