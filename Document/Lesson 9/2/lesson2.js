f67fa9fdf89d4cbf8fd9a110c695ef14ea7721e5
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2024-04-05 01:32:56 +0300

    lesson 9

---
+++ Document/Lesson 9/2/lesson2.js
@@ -0,0 +1,6 @@
+function findScrollBarWidth(elem) {
+  let computedStyleElem = getComputedStyle(elem)
+  alert(computedStyleElem.height.replace('px', '') - (elem.clientHeight - 2 * computedStyleElem.padding.replace('px', '')));
+}
+
+findScrollBarWidth(example);
